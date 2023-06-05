5ea2b927c8ae4881e9633adb02e6823eddc05c0d
Jarno Ojasaar <jarno.ojasaar@gmail.com>
2023-06-05 21:14:54 +0300

    todos page shows todos as a list from mongodb obj

--- app/todos/page.jsx
+++ app/todos/page.jsx
@@ -1,15 +1,35 @@
-import { getTodos } from "@lib/mongodb/todos";
+"use client";
+import useSWR from "swr";
 
-async function fetchTodos() {
-  const { todos } = await getTodos();
-  if (!todos) throw new Error("Failed to fetch movies");
+// import { getTodos } from "@lib/mongodb/todos";
 
-  return todos;
-}
+// async function fetchTodos() {
+//   const { todos } = await getTodos();
+//   if (!todos) throw new Error("Failed to fetch movies");
 
-export default async function Home() {
-  const todos = await fetchTodos();
+//   return todos;
+// }
+
+export default function Home() {
+  const fetcher = (url) => fetch(url).then((r) => r.json());
+  const { data, error, isLoading } = useSWR(
+    "http://localhost:3000/api/todos",
+    fetcher
+  );
+  if (error) return <div className="text-5xl">failed to load</div>;
+  if (isLoading) return <div className="text-5xl">loading...</div>;
+
+  // ! gets todos from mongodb Object
+  let todos = Object.entries(data);
+  todos = todos[0][1];
   console.log(todos);
+
+  // const todos = await fetchTodos();
+  // console.log(todos[2].name);
+  // let changeTodos = Object.entries(todos);
+  // changeTodos = Object.entries(changeTodos[0][1]);
+  // changeTodos = Object.entries(changeTodos[1][1][0]);
+  // console.log(Object.entries(changeTodos));
   return (
     <div>
       <ul>
